247d21b5ecbae07cafbf576c41fbab17299fbdbd
Tiziano Rullo <tiziano.rullo@liip.ch>
2017-04-20 12:08:41 +0200

    persist sorting

--- src/middlewares/firebase.js
+++ src/middlewares/firebase.js
@@ -1,7 +1,8 @@
 import firebase from 'firebase'
+import {arrayMove} from 'react-sortable-hoc'
 
 import {PROJECT_ADD, PROJECT_FETCH} from '../actions/project'
-import {FEATURE_UPDATE, FEATURE_DELETE} from '../actions/feature'
+import {FEATURE_UPDATE, FEATURE_DELETE, FEATURE_SORT} from '../actions/feature'
 import {BUDGET_UPDATE} from '../actions/budget'
 import {RATE_UPDATE} from '../actions/rate'
 import {NAME_UPDATE} from '../actions/name'
@@ -35,6 +36,13 @@ const firebaseMiddleware = store => next => action => {
     case FEATURE_DELETE:
       db.ref('/projects/' + store.getState().selectedProjectId + '/features/' + action.payload.id)
         .remove()
+        .then(next(action))
+      break
+    case FEATURE_SORT:
+      const selectedProjectId = store.getState().selectedProjectId
+      const features = arrayMove(store.getState().projects[selectedProjectId].features, action.payload.oldIndex, action.payload.newIndex)
+      db.ref('/projects/' + selectedProjectId + '/features/')
+        .set(features)
         .then(next(action))
       break
     case BUDGET_UPDATE:
